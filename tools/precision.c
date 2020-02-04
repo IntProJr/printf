2b567bd2674733d2ad65d93b50aa0c064cb7e37e
intpro <intprojr@yandex.ru>
2020-02-05 00:00:56 +0300

    fix precision.c

--- tools/precision.c
+++ tools/precision.c
@@ -12,18 +12,13 @@
 
 #include "../ft_printf.h"
 
-int	precision(char *str)
+int precision(char *str)
 {
-	int		i;
-	int		num_tmp;
-	int		j;
+	int       i;
+	char   tmp[ft_strlen(str)];
+	int       j;
 
-	num_tmp = ft_strlen(str);
-	if (num_tmp == 0)
-		return (-1);
-	char	tmp[num_tmp];
 	i = 0;
-	j = 0;
 	ft_memset(tmp, '\0', ft_strlen(str));
 	while (str[i] != '\0')
 	{
@@ -38,7 +33,39 @@ int	precision(char *str)
 		else
 			i++;
 	}
-	if (ft_strchr(str, '.') && (ft_strcmp(tmp, "0") == 0 || tmp[0] == '\0'))
+	if (ft_strchr(str, '.') && ((ft_strcmp(tmp, "0") == 0 || ft_strcmp(tmp, "00") == 0 ||
+								 ft_strcmp(tmp, "000") == 0 || ft_strcmp(tmp, "0000") == 0 ||
+								 ft_strcmp(tmp, "00000") == 0) || tmp[0] == '\0'))
 		return (-1);
 	return (ft_atoi(tmp));
 }
+//int	precision(char *str)
+//{
+//	int		i;
+//	int		num_tmp;
+//	int		j;
+//
+//	num_tmp = ft_strlen(str);
+//	if (num_tmp == 0)
+//		return (-1);
+//	char	tmp[num_tmp];
+//	i = 0;
+//	j = 0;
+//	ft_memset(tmp, '\0', ft_strlen(str));
+//	while (str[i] != '\0')
+//	{
+//		if (str[i] == '.')
+//		{
+//			ft_memset(tmp, '\0', ft_strlen(str));
+//			j = 0;
+//			i++;
+//			while (ft_strchr("0123456789", str[i]) && str[i] != '\0')
+//				tmp[j++] = str[i++];
+//		}
+//		else
+//			i++;
+//	}
+//	if (ft_strchr(str, '.') && (ft_strcmp(tmp, "0") == 0 || tmp[0] == '\0'))
+//		return (-1);
+//	return (ft_atoi(tmp));
+//}
